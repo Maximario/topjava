cc06e4724670357fe50900e6253d343e753790f9
Maximario <grevtsovm@yandex.ru>
2020-06-26 12:07:51 +0300

    InitDB and populateDB scripts for meals

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
@@ -9,3 +10,13 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (user_id, date_time, description, calories) VALUES
+    (100000, '2020-06-25 10:00', 'Завтрак', 1000),
+    (100000, '2020-06-25 14:00', 'Обед', 2000),
+    (100000, '2020-06-25 20:00', 'Ужин', 2000),
+    (100000, '2020-06-26 00:00', 'Поздний ужин', 1000),
+    (100001, '2020-06-26 00:00', 'Завтрак', 500),
+    (100001, '2020-06-26 09:00', 'Завтрак', 500),
+    (100001, '2020-06-26 15:00', 'Обед', 500),
+    (100001, '2020-06-26 20:00', 'Ужин', 400);
