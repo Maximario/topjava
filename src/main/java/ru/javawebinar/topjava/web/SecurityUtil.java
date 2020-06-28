567013e7b542f93455fdf1f3779daf3c0d7d0ac9
Maximario <grevtsovm@yandex.ru>
2020-06-29 02:17:22 +0300

    Change SecurityUtil for user select

--- src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
+++ src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
@@ -1,14 +1,22 @@
 package ru.javawebinar.topjava.web;
 
-import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
+import static ru.javawebinar.topjava.util.UsersUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class SecurityUtil {
 
+    private static int authUserId;
+
     public static int authUserId() {
-        return 1;
+        return authUserId;
+    }
+
+    public static void setAuthUserId(int authUserId) {
+        SecurityUtil.authUserId = authUserId;
     }
 
     public static int authUserCaloriesPerDay() {
         return DEFAULT_CALORIES_PER_DAY;
     }
+
+
 }
