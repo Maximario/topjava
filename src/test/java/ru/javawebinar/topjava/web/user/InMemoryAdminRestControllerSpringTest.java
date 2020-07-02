fbc5198a31aa807761e1d8a05b6804af0cfcae0b
Maximario <grevtsovm@yandex.ru>
2020-07-02 04:21:54 +0300

    Fix InMemory Test

--- src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
@@ -13,10 +13,7 @@ import ru.javawebinar.topjava.util.exception.NotFoundException;
 import static ru.javawebinar.topjava.web.UserTestData.NOT_FOUND;
 import static ru.javawebinar.topjava.web.UserTestData.USER_ID;
 
-@ContextConfiguration({
-        "classpath:spring/spring-app.xml",
-        "classpath:spring/inmemory.xml"
-})
+@ContextConfiguration("classpath:spring/inmemory.xml")
 @RunWith(SpringRunner.class)
 public class InMemoryAdminRestControllerSpringTest {
 
